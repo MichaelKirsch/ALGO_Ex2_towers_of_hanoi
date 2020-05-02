5b74617046129a46557e83cde9faa694ac6e860a
Michael <michael_kirsch>
2020-05-02 18:32:23 +0200

    assign

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
+#define DEBUG false
 
 void move(int& count,int n, int src, int work, int dest)
 {
     count++;
     if(n>1)
         move(count,n-1,src,dest,work);
-    std::cout <<"Step:"<<count << " | Move disk " << n << " from " << src << " to " << dest<< std::endl;
+    if(DEBUG)
+        std::cout <<"Step:"<<count << " | Move disk " << n << " from " << src << " to " << dest<< std::endl;
     if(n>1)
         move(count,n-1,work,src,dest);
 }
 
-int main() {
-
-    int n = 10;
+int call_the_towers(int amount)
+{
+    int n = amount;
     int dest = 3;
-    int counter =0;
+    int counter = 0;
 
     move(counter,n,1,dest-1,dest);
 
-    std::cout << "Took " << counter << " steps" << std::endl;
+    std::cout << "Took " << counter << " steps for "<< amount << std::endl;
+    return counter;
+}
+
+
+
+int main() {
+    call_the_towers(3);
+    call_the_towers(4);
+    call_the_towers(5);
+
     return 0;
 }
